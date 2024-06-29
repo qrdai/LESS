3ac157b62e0ea752344ae14f70d0ce17ac0ce92a
Qirun Dai <qirundai@sina.com>
2024-06-29 16:41:10 -0500

    update install and download for uiuc-cc

--- hf_download.py
+++ hf_download.py
@@ -1,16 +1,14 @@
 import os
-os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+# os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 
 from transformers import AutoTokenizer, AutoModelForCausalLM
 
-# Define the model name and the target directory for caching
-# model_name_or_path = 'meta-llama/Llama-2-7b-hf'  # replace with your model name
+# model_name_or_path = 'meta-llama/Llama-2-7b-hf'
 model_name_or_path = 'TinyLlama/TinyLlama-1.1B-intermediate-step-1431k-3T'
-cache_dir = '/root/autodl-tmp/huggingface/transformers'
+# cache_dir = '/root/autodl-tmp/huggingface/transformers'   # for autodl
+cache_dir = '/projects/illinois/eng/cs/haopeng/qirundai/.cache/huggingface/transformers'   # for CC at UIUC
 
-# Ensure the cache directory exists
 os.makedirs(cache_dir, exist_ok=True)
 
-# Download and cache the tokenizer and model
 tokenizer = AutoTokenizer.from_pretrained(model_name_or_path, cache_dir=cache_dir)
 model = AutoModelForCausalLM.from_pretrained(model_name_or_path, cache_dir=cache_dir)
--- hf_download_dataset.py
+++ hf_download_dataset.py
@@ -1,13 +1,13 @@
 import os
-os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
+# os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 from datasets import load_dataset
-cache_dir = '/root/autodl-tmp/huggingface/datasets'
-dataset_name_or_path = 'databricks/databricks-dolly-15k'
 
-# Ensure the cache directory exists
+dataset_name_or_path = 'databricks/databricks-dolly-15k'
+# cache_dir = '/root/autodl-tmp/huggingface/datasets' # for autodl
+cache_dir = '/projects/illinois/eng/cs/haopeng/qirundai/.cache/huggingface/datasets'    # for CC at UIUC
+
 os.makedirs(cache_dir, exist_ok=True)
 
-# Load the dataset from the specified mirror and cache it in the custom directory
 dataset = load_dataset(dataset_name_or_path, cache_dir=cache_dir)
 
 print(dataset["train"][:10])
